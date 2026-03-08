d2c343c9f1453125b79b202c8a3589ad4c08d4d0
Kelvin Atsu Djayouri <djayourikelvinatsu@gmail.com>
2026-03-08 00:01:30 +0000

    feat: add contact link to navbar

--- src/components/layout/navbar.tsx
+++ src/components/layout/navbar.tsx
@@ -15,6 +15,7 @@ const navItems = [
     { name: "Home", path: "/" },
     { name: "About", path: "/about" },
     { name: "Projects", path: "/projects" },
+    { name: "Contact", path: "/#contact" },
 ]
 
 export function Navbar() {
